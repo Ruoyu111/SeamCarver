01c2024a63a4570191683e1241180560a5c266aa
Ruoyu Chen <chen.ruo@husky.neu.edu>
2018-03-31 11:52:07 -0400

    Added energy function.

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -28,15 +28,51 @@ public class SeamCarver {
     
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        
+        validatePixel(x, y);
+        if (atBorder(x, y)) 
+            return 1000.00;
+        return Math.sqrt(squareGradient(x - 1, y, x + 1, y) + squareGradient(x, y - 1, x, y + 1));
     }
     
     // helper functions
     
-    // calculate central difference between pixel(x1, y1) and pixel(x2, y2)
+    private void validatePixel(int x, int y) {
+        if (x < 0 || x >= width() || y < 0 || y >= height()) 
+            throw new IllegalArgumentException("Pixel is outside its prescribed range");
+    }
+    
+    // check whether the pixel is at the border
+    private boolean atBorder(int x, int y) {
+        if (x == 0 || x == width() - 1 || y == 0 || y == height() - 1) 
+            return true;
+        return false;
+    }
+    
+    // calculate square gradient between pixel(x1, y1) and pixel(x2, y2)
     // which is the quare sum of differences in the red, green and blue components
-    private int gradient(int x1, int y1, int x2, int y2) {
-        
+    private int squareGradient(int x1, int y1, int x2, int y2) {
+        // pixel1 rgb value
+        int[] rgb1 = getRGB(x1, y1);
+        // pixel2 rgb value
+        int[] rgb2 = getRGB(x2, y2);
+        int res = 0;
+        for (int i = 0; i < 3; i++) 
+            res += centralDiff(rgb1[i], rgb2[i]);
+        return res;
+    }
+    
+    // calculate square difference
+    private int centralDiff(int c1, int c2) {
+        return (c1 - c2) * (c1 - c2);
+    }
+    
+    // get rgb value of a pixel
+    private int[] getRGB(int x, int y) {
+        int rgb = picture.getRGB(x, y);
+        int r = (rgb>>16)&0XFF;
+        int g = (rgb>>8)&0XFF;
+        int b = (rgb>>0)&0XFF;
+        return new int[] {r, g, b};
     }
 
     public static void main(String[] args) {
