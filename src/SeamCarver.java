2903ea85609b214334e970e936b35b6d5913a7cf
Ruoyu Chen <chen.ruo@husky.neu.edu>
2018-03-31 19:37:06 -0400

    Added findVerticalSeam

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -34,6 +34,68 @@ public class SeamCarver {
         return Math.sqrt(squareGradient(x - 1, y, x + 1, y) + squareGradient(x, y - 1, x, y + 1));
     }
     
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        
+    }
+    
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        // construct a 2d energy array
+        int width = width();
+        int height = height();
+        double[][] energyMatrix = new double[width][height];
+        double[][] distTo = new double[width][height];
+        int[][] edgeTo = new int[width][height];
+        // initialize above matrixes
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                energyMatrix[x][y] = energy(x, y);
+                
+                if (y == 0) {
+                    // first line, sources
+                    distTo[x][y] = energyMatrix[x][y];
+                } else {
+                    distTo[x][y] = Double.POSITIVE_INFINITY;
+                }
+                
+                edgeTo[x][y] = -1;
+            }
+        }
+        
+        // topological order
+        for (int y = 0; y < height - 1; y++) {
+            for (int x = 0; x < width; x++) {
+                // relax each edge (x, y)
+                for (int i = -1; i <= 1; i++) {
+                    if (x + i < 0 || x + i >= width) continue;
+                    else if (distTo[x + i][y + 1] > distTo[x][y] + energyMatrix[x + i][y + 1]) {
+                        // update
+                        distTo[x + i][y + 1] = distTo[x][y] + energyMatrix[x + i][y + 1];
+                        edgeTo[x + i][y + 1] = x;
+                    }
+                }
+            }
+        }
+        
+        // find seam endpoint
+        int end = 0;
+        for (int x = 1; x < width; x++) {
+            if (distTo[x][height - 1] < distTo[end][height - 1]) {
+                end = x;
+            }
+        }
+        
+        // build return array
+        int[] res = new int[height];
+        res[height - 1] = end;
+        for (int i = height - 2; i >= 0; i--) {
+            res[i] = edgeTo[end][i + 1];
+            end = res[i];
+        }
+        return res;
+    }
+    
     // helper functions
     
     private void validatePixel(int x, int y) {
